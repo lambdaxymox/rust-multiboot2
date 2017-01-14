9bce11556e83650338da9e7a90d7d3959be1dc39
LambdaXymox <lambda.xymox@gmail.com>
2017-01-13 22:19:44 -0600

    Add tag iterator to fetch multiboot tag information.

--- src/tag.rs
+++ src/tag.rs
@@ -1,38 +1,47 @@
 #![allow(dead_code)]
-use core::mem;
-
 /// A Multiboot tag structure is just a queryable blob of bytes. The implementation presently
 /// assumes that the size is at least 8 bytes (for the end tag), and does not check this.
-const BOOT_COMMAND_LINE: u32   = 1;
-const BOOT_LOADER_NAME: u32    = 2;
-const MODULE: u32            = 3;
-const MEMORY_INFORMATION: u32 = 4;
-const BIOS_BOOT_DEVICE: u32    = 5;
-const MEMORY_MAP: u32         = 6;
-const VBE_INFO: u32           = 7;
-const FRAME_BUFFER_INFO: u32   = 8;
-const ELF_SYMBOLS: u32        = 9;
-const APM_TABLE: u32          = 10;
+pub enum TagType {
+	BootCommandLine   = 1,
+	BootLoaderName    = 2,
+	Module            = 3,
+	MemoryInformation = 4,
+	BiosBootDevice    = 5,
+	MemoryMap         = 6,
+	VBEInfo           = 7,
+	FrameBufferInfo   = 8,
+	ElfSymbols        = 9,
+	APMTable          = 10,
+}
 
 
 #[repr(C)]
 struct Tag {
-	// Pointer to the first byte of the Tag block.
-	ptr: *const u8,
+	tag_type: u32,
+	size: u32,
+	// The tag data follows these two fields.
 }
 
-impl Tag {
-	fn tag_type(&self) -> u32 {
-		let ptr = self.ptr as *const u32;
-		unsafe {
-			*ptr
-		}
-	}
+struct TagIter {
+	current: *const Tag
+}
 
-	fn size(&self) -> u32 {
-		let ptr = ((self.ptr as usize) + 4) as *const u32;
-		unsafe {
-			*ptr
+impl Iterator for TagIter {
+	type Item = &'static Tag;
+
+	fn next(&mut self) -> Option<&'static Tag> {
+		let current = unsafe { &*self.current };
+		match current {
+			&Tag { tag_type: 0, size: 8 } => None, // End tag.
+			tag => {
+				// Jump to the next tag.
+				let mut tag_address = self.current as usize;
+				tag_address += tag.size as usize;
+				tag_address = ((tag_address - 1) & !0x07) + 0x08;
+				self.current = tag_address as *const Tag;
+
+				Some(tag)
+			}
 		}
 	}
 }
