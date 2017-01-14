c9dc796d6d740dff16c67774fe8b6d673aab90b3
LambdaXymox <lambda.xymox@gmail.com>
2017-01-13 21:50:29 -0600

    Define Tag structure.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,3 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
+#![no_std]
+
+mod tag;
---
+++ src/tag.rs
@@ -0,0 +1,42 @@
+#![allow(dead_code)]
+use core::mem;
+
+/// A Multiboot tag structure is just a queryable blob of bytes. The implementation presently
+/// assumes that the size is at least 8 bytes (for the end tag), and does not check this.
+const BOOT_COMMAND_LINE: u32   = 1;
+const BOOT_LOADER_NAME: u32    = 2;
+const MODULE: u32            = 3;
+const MEMORY_INFORMATION: u32 = 4;
+const BIOS_BOOT_DEVICE: u32    = 5;
+const MEMORY_MAP: u32         = 6;
+const VBE_INFO: u32           = 7;
+const FRAME_BUFFER_INFO: u32   = 8;
+const ELF_SYMBOLS: u32        = 9;
+const APM_TABLE: u32          = 10;
+
+type TagType = u32;
+
+type TagSize = u32;
+
+
+#[repr(C)]
+struct Tag {
+	// Pointer to the first byte of the Tag block.
+	ptr: *const u8,
+}
+
+impl Tag {
+	fn tag_type(&self) -> TagType {
+		let ptr = self.ptr as *const u32;
+		unsafe {
+			*ptr
+		}
+	}
+
+	fn size(&self) -> TagSize {
+		let ptr = ((self.ptr as usize) + 4) as *const u32;
+		unsafe {
+			*ptr
+		}
+	}
+}
