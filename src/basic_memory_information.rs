d0d1d16ed72a8bb7115ee664f4455b2a5c0dbb16
LambdaXymox <lambda.xymox@gmail.com>
2017-01-13 23:44:56 -0600

    Define the tag for describing basic memory information.

--- src/basic_memory_information.rs
+++ src/basic_memory_information.rs
@@ -1,1 +1,15 @@
+use tag::TagType;
 
+
+struct BasicMemoryInformationTag {
+	tag_type: u32,
+	size: u32,
+	mem_lower: u32,
+	mem_upper: u32,
+}
+
+impl BasicMemoryInformationTag {
+	fn is_valid(&self) -> bool {
+		(self.mem_lower <= 640) && (self.tag_type == TagType::MemoryInformation as u32)
+	}
+}
